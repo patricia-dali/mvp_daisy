006eef24f5c7bbdc7d19d46d9fccf210c95924c7
patricia-dali <patricia.dali2022@gmail.com>
2024-06-25 14:57:20 -0300

    Att.

--- src/index/index.go
+++ src/index/index.go
@@ -152,7 +152,7 @@ func questionDB(db *sql.DB, isAdmin bool, response string) ([]map[string]interfa
 	if isAdmin {
 		query = response
 	} else {
-		query = "SELECT * FROM PRODUTOS"
+		query = "SELECT * FROM PRODUTOS LIMIT 5"
 	}
 
 	rows, err := db.Query(query)
